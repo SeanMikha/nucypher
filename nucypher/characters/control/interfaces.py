c02d098c3ff32aafd86624b16e32d854b11bee3f
jMyles <justin@justinholmes.com>
2019-08-22 09:55:47 +0200

    Bob will learn on the main thread during CLI commands until #1234 is fixed.

--- nucypher/characters/control/interfaces.py
+++ nucypher/characters/control/interfaces.py
@@ -213,7 +213,7 @@ class BobInterface(CharacterPublicInterface, BobSpecification):
                                               label=label)
 
         self.character.join_policy(label=label, alice_verifying_key=alice_verifying_key)
-        plaintexts = self.character.retrieve(message_kit=message_kit,
+        plaintexts = self.character.retrieve(message_kit,
                                        enrico=data_source,
                                        alice_verifying_key=alice_verifying_key,
                                        label=label)
